2d735b157f7299b9f31224b896b12357f4fbba50
Pedro <pmg.reis@campus.fct.unl.pt>
2021-12-30 18:52:08 +0000

    Code improvements
    Finding code smells and correcting them. Fixed some bugs.

--- jabref/src/main/java/org/jabref/logic/importer/fileformat/CSVImporter.java
+++ jabref/src/main/java/org/jabref/logic/importer/fileformat/CSVImporter.java
@@ -2,14 +2,11 @@ package org.jabref.logic.importer.fileformat;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
-import org.jabref.logic.JabRefException;
-import org.jabref.logic.importer.ImportException;
 import org.jabref.logic.importer.Importer;
 import org.jabref.logic.importer.ParserResult;
 import org.jabref.logic.l10n.Localization;
 import org.jabref.logic.util.StandardFileType;
 import org.jabref.model.entry.BibEntry;
-import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
 import org.jabref.model.entry.types.StandardEntryType;
 
@@ -18,7 +15,22 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.util.*;
 
+/**
+ * This class represents the CSV importer feature added during the project.
+ * It allows the user to import more bibliographic references from a file
+ * with csv extension into their JabRef application.
+ *
+ * @authors Clara Sousa, Paula Ines Lopes, Pedro Reis, Ricardo Pereira, Rita Silva
+ */
+
 public class CSVImporter extends Importer {
+
+    @Override
+    public String getId() { return "CSV"; }
+
+    @Override
+    public String getDescription() { return "Wraps the CSV function to be used as an Importer."; }
+
     @Override
     public String getName() {
         return "CSV";
@@ -31,7 +43,6 @@ public class CSVImporter extends Importer {
 
     @Override
     public ParserResult importDatabase(BufferedReader reader) throws IOException {
-        //Objects.requireNonNull(reader);
         throw new UnsupportedOperationException(
                 "CSVImporter does not support importDatabase(BufferedReader reader)."
                         + "Instead use importDatabase(Path filePath, Charset defaultEncoding).");
@@ -39,14 +50,21 @@ public class CSVImporter extends Importer {
 
     @Override
     public ParserResult importDatabase(String data) throws IOException {
-        //Objects.requireNonNull(data);
         throw new UnsupportedOperationException(
                 "CSVImporter does not support importDatabase(String data)."
                         + "Instead use importDatabase(Path filePath, Charset defaultEncoding).");
     }
 
-    public ArrayList<BibEntry> readCSV(Path filePath) {
-        ArrayList<BibEntry> csvEntries = new ArrayList<>(100);
+    /**
+     * Gets a list of bibliographic references present in the given csv file,
+     * provided that they have the format: Type,Author,Title,Year,Journal
+     *
+     * @param filePath path of the csv file
+     * @return List<BibEntry> list of entries to be imported
+     * @Pre: The provided path refers to a csv file format
+     */
+    public List<BibEntry> readCSV(Path filePath) {
+        List<BibEntry> csvEntries = new ArrayList<>(100);
         Reader csv;
 
         try {
@@ -65,34 +83,44 @@ public class CSVImporter extends Importer {
 
         BibEntry entry;
 
-        for (CSVRecord record : records) {
-            if (record.size() == 5) {
+        for (CSVRecord csvRecord : records) {
+            if (csvRecord.size() == 5) {
                 entry = new BibEntry();
-                entry.setType(StandardEntryType.valueOf(record.get(0)));
-                entry.setField(StandardField.AUTHOR, record.get(1));
-                entry.setField(StandardField.TITLE, record.get(2));
-                entry.setField(StandardField.YEAR, record.get(3));
-                entry.setField(StandardField.JOURNAL, record.get(4));
+                entry.setType(StandardEntryType.valueOf(csvRecord.get(0)));
+                entry.setField(StandardField.AUTHOR, csvRecord.get(1));
+                entry.setField(StandardField.TITLE, csvRecord.get(2));
+                entry.setField(StandardField.YEAR, csvRecord.get(3));
+                entry.setField(StandardField.JOURNAL, csvRecord.get(4));
 
                 csvEntries.add(entry);
             }
         }
 
-        Collections.sort(csvEntries, new Comparator<BibEntry>() {
-            public int compare(BibEntry b1, BibEntry b2) {
-                return b1.getField(StandardField.AUTHOR).get().compareTo(b2.getField(StandardField.AUTHOR).get());
-            }
+        Collections.sort(csvEntries, (b1, b2) -> {
+            Optional<String> author1 = b1.getField(StandardField.AUTHOR);
+            Optional<String> author2 = b2.getField(StandardField.AUTHOR);
+            if (author1.isPresent() && author2.isPresent()) {
+                return author1.get().compareTo(author2.get());
+            } else return 0;
         });
 
         return csvEntries;
     }
 
+    /**
+     * Imports the bibliographic references
+     *
+     * @param filePath        the path to the file which should be imported
+     * @param defaultEncoding the text encoding being used
+     * @return ParserResult parsed entries
+     * @throws IOException if the list of entries is empty or if an I/O exception occurs.
+     */
     @Override
     public ParserResult importDatabase(Path filePath, Charset defaultEncoding) throws IOException {
         Objects.requireNonNull(filePath);
         try {
-            ArrayList<BibEntry> entries = readCSV(filePath);
-            if (entries.size() == 0) {
+            List<BibEntry> entries = readCSV(filePath);
+            if (entries.isEmpty()) {
                 throw new IOException();
             } else {
                 return new ParserResult(entries);
@@ -109,26 +137,20 @@ public class CSVImporter extends Importer {
     }
 
     /**
-     * Returns whether the given stream contains data that is a.) a CSV and b.)
-     * contains at least one BibEntry.
+     * Checks whether the provided file path corresponds to a csv file
+     *
+     * @param filePath        the path of the file to check
+     * @param defaultEncoding the text encoding being used
+     * @return true if the file has a csv extension, false if otherwise
+     * @throws IOException if an I/O exception occurs.
      */
     @Override
     public boolean isRecognizedFormat(Path filePath, Charset defaultEncoding) throws IOException {
         String fileExtension = filePath.toString().split("\\.")[1];
+        boolean recognized = false;
+        if (fileExtension.equals("csv")) recognized = true;
 
-        if (fileExtension.equals("csv"))
-            return true;
-        else
-            return false;
+        return recognized;
     }
 
-    @Override
-    public String getId() {
-        return "CSV";
-    }
-
-    @Override
-    public String getDescription() {
-        return "Wraps the CSV function to be used as an Importer.";
-    }
 }
--- jabref/src/test/java/org/jabref/logic/importer/fileformat/CSVImporterTest.java
+++ jabref/src/test/java/org/jabref/logic/importer/fileformat/CSVImporterTest.java
@@ -7,33 +7,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class CSVImporterTest {
+class CSVImporterTest {
 
     private CSVImporter importer;
 
     @BeforeEach
-    public void setUp() throws Exception {
+    void setUp() throws Exception {
         importer = new CSVImporter();
     }
 
     @Test
-    public void testGetFormatName() {
+    void testGetFormatName() {
         assertEquals("CSV", importer.getName());
     }
 
     @Test
-    public void testGetCLIId() {
+    void testGetCLIId() {
         assertEquals("CSV", importer.getId());
     }
 
     @Test
-    public void testsGetExtensions() {
+    void testsGetExtensions() {
         assertEquals(StandardFileType.CSV, importer.getFileType());
     }
 
     @Test
-    public void testGetDescription() {
+    void testGetDescription() {
         assertEquals("Wraps the CSV function to be used as an Importer.", importer.getDescription());
     }
-    
+
 }
